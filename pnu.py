54a6955eef5763938bbac74dc0fa92a1a7ac7b8e
SHZoo <wntjdgus02@pusan.ac.kr>
2022-07-14 00:26:46 +0900

    ...

--- pnu.py
+++ pnu.py
@@ -1,9 +1,19 @@
-n ,m = map(int, input().split())
+numbers = [1, 2, 3, 4, 5]
+iterator = reversed(numbers)
 
-result = 0
-for i in range(n) :
-    data = list(map(int, input().split()))
-    min_value = min(data)
-    result = max(min_value, result)
+ # 메모리를 절약할 수 있다.
+for i in iterator :
+    print(i)
 
-print(result)
+for i in iterator :
+    print(i)
+
+for i in iterator :
+    print(i)
+
+def 반전(리스트) :
+    for i in range(len(리스트)):
+        yield 리스트[-i -1]
+    
+반전([1, 2, 3, 4, 5, 6])
+
