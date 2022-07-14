75d5ea7d20a4d61d97f2ff1028e82aa4aae75cdc
SHZoo <wntjdgus02@pusan.ac.kr>
2022-07-14 15:10:34 +0900

    solve backjoon 10172

--- baekjoon/level01/Code04.py
+++ baekjoon/level01/Code04.py
@@ -1,11 +1,7 @@
-a = int(input())
-b = int(input())
-
-if a > 0 and b > 0 :
-    print(1)
-elif a > 0 and b < 0 :
-    print(4)
-elif a < 0 and b > 0 :
-    print(2)
-else :
-    print(3)
+print("""
+|\_/|
+|q p|   /}
+( 0 )\"\"\"\\
+|"^"`    |
+||_/=\\\\__|
+""".strip())
