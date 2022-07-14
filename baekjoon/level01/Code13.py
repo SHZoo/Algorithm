9cd587a28473c55cb81765f0f00a27ada26fc2d2
SHZoo <wntjdgus02@pusan.ac.kr>
2022-07-14 15:14:47 +0900

    solve backjoon 2588

---
+++ baekjoon/level01/Code13.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+print(a * int(str(b)[2]))
+print(a * int(str(b)[1]))
+print(a * int(str(b)[0]))
+print(a * int(str(b)[2]) + (a * int(str(b)[1])) * 10 + (a * int(str(b)[0])) * 100)
