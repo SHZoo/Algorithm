d31b0f479c113ae6fe281839f76b37a621f61d7c
SHZoo <wntjdgus02@pusan.ac.kr>
2022-07-16 23:41:51 +0900

    Breadth First Search

---
+++ ps/Ch05/bfs.py
@@ -0,0 +1,31 @@
+# 너비 우선 탐색(bfs)
+from collections import deque
+
+def bfs(graph, start, visited) :
+    queue = deque([start])
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue :
+        v = queue.popleft()
+        print(v, end = ' ')
+        for i in graph[v] :
+            if not visited[i] :
+                queue.append(i)
+                visited[i] = True
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False for _ in range(9)]
+
+bfs(graph, 1, visited)
+# 1 2 3 8 7 4 5 6
