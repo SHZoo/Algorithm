2b23ba55f38c3a3dbf29551ab7159c005116bb21
SHZoo <wntjdgus02@pusan.ac.kr>
2022-08-26 12:39:55 +0900

    "solve baekjoon 5014"

---
+++ baekjoon/BFS/5014.py
@@ -0,0 +1,30 @@
+# boj 5014 스타트링크
+import heapq
+
+f, s, g, u, d = map(int, input().split())
+visited = [False] * (1000001)
+
+def bfs(start):
+    hq = []
+    heapq.heappush(hq, (0, start))
+
+    while hq:
+        cnt, now = heapq.heappop(hq)
+        if now == g:
+            return cnt
+        if now <= 0 or now > f:
+            continue
+        if visited[now]:
+            continue
+        visited[now] = True
+        heapq.heappush(hq, (cnt+1, now + u))
+        heapq.heappush(hq, (cnt+1, now - d))
+
+    return None
+
+res = bfs(s)
+
+if res == None :
+    print("use the stairs")
+else :
+    print(res)
