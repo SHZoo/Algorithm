536eb0b93e920c9bf8af9803e995776b142e08d1
SHZoo <wntjdgus02@pusan.ac.kr>
2022-08-26 12:48:05 +0900

    solve baekjoon 1389

--- baekjoon/Floyd-Warshall/1389.py
+++ baekjoon/Floyd-Warshall/1389.py
@@ -5,28 +5,28 @@ graph = [[INF] * (n+1) for _ in range(n+1)]
 
 for i in range(1, n+1) :
     for j in range(1, n+1) :
-        if i == j :
+        if i == j:
             graph[i][j] = 0
 
-for _ in range(m) :
+for _ in range(m):
     u, v = map(int, input().split())
     graph[u][v] = 1
     graph[v][u] = 1
 
-for k in range(1, n+1) :
-    for i in range(1, n+1) :
-        for j in range(1, n+1) :
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
             graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
 
 Kevin_Bacon = INF
 res_list = []
 
-for i in range(1, n+1) :
-    if sum(graph[i][1:]) < Kevin_Bacon :
+for i in range(1, n+1):
+    if sum(graph[i][1:]) < Kevin_Bacon:
         res_list.clear()
         res_list.append(i)
         Kevin_Bacon = sum(graph[i][1:])
-    elif sum(graph[i][1:]) == Kevin_Bacon :
+    elif sum(graph[i][1:]) == Kevin_Bacon:
         res_list.append(i)
 
 res_list.sort()
