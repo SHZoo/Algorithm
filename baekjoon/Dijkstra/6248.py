5fa2823fb8b6c7306ba10947a3526e97d946dede
SHZoo <wntjdgus02@pusan.ac.kr>
2022-09-21 20:54:26 +0900

    solve baekjoon 6248

---
+++ baekjoon/Dijkstra/6248.py
@@ -0,0 +1,31 @@
+# boj 6248 Bronze Cow Party
+import heapq
+
+INF = (1e10)
+n, m, start = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    u, v, c= map(int, input().split())
+    graph[u].append((v, c))
+    graph[v].append((u, c))
+
+def dijkstra(start):
+    hq = []
+    heapq.heappush(hq, (0, start))
+    distance[start] = 0
+
+    while hq:
+        dist, now = heapq.heappop(hq)
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if distance[i[0]] > cost:
+                distance[i[0]] = cost
+                heapq.heappush(hq, (cost, i[0]))
+
+dijkstra(start)
+print(max(distance[1:])*2)
